c45b88b5b842f58e98b14b269c159d3f7a5a08f3
Christopher Watkins <100959728+cwatkins-DL@users.noreply.github.com>
2026-06-05 10:23:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_repsonse)
 
 # Write directly to the app
